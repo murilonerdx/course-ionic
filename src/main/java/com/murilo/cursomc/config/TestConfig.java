b94d8162b6ffe729e592a44604abf5ac5d794e70
n30config <mu-silva@outlook.com>
2021-03-09 15:15:14 -0300

    Adicionando configurações test e dev

--- src/main/java/com/murilo/cursomc/config/TestConfig.java
+++ src/main/java/com/murilo/cursomc/config/TestConfig.java
@@ -25,12 +25,14 @@ import com.murilo.cursomc.model.produto.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 @Configuration
-public class CommandLine implements CommandLineRunner {
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
