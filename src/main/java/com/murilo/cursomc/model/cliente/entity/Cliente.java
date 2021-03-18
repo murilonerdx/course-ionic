28fb60b6806f6a912c8938596833ec8841504091
Murilonerdx <mu-silva@outlook.com>
2021-03-18 16:33:49 -0300

    Salvando perfis de usuario

--- src/main/java/com/murilo/cursomc/model/cliente/entity/Cliente.java
+++ src/main/java/com/murilo/cursomc/model/cliente/entity/Cliente.java
@@ -5,152 +5,166 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.murilo.cursomc.model.cliente.enums.Perfil;
 import com.murilo.cursomc.model.cliente.enums.TipoCliente;
 import com.murilo.cursomc.model.endereco.entity.Endereco;
 import com.murilo.cursomc.model.pedido.entity.Pedido;
-import org.hibernate.validator.constraints.Length;
 
 @Entity
 public class Cliente implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Integer id;
-	private String nome;
-	@Column(unique = true)
-	private String email;
-	private String cpfOuCnpj;
-	private Integer tipo;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
 
-	@JsonIgnore
-	private String senha;
-	
-	@OneToMany(mappedBy="cliente")
-	private List<Endereco> enderecos = new ArrayList<>();
-	
-	@ElementCollection
-	@CollectionTable(name="TELEFONE")
-	private Set<String> telefones = new HashSet<>();
-	
-	@JsonIgnore
-	@OneToMany(mappedBy="cliente", cascade=CascadeType.ALL)
-	private List<Pedido> pedidos = new ArrayList<>();
-	
-	public Cliente() {
-	}
+    @Column(unique = true)
+    private String email;
+    private String cpfOuCnpj;
+    private Integer tipo;
 
-	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
-		super();
-		this.id = id;
-		this.senha = senha;
-		this.nome = nome;
-		this.email = email;
-		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = (tipo==null) ? null : tipo.getCod();
-	}
+    @JsonIgnore
+    private String senha;
 
-	public String getSenha() {
-		return senha;
-	}
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<Endereco> enderecos = new ArrayList<>();
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
+    @ElementCollection
+    @CollectionTable(name = "TELEFONE")
+    private Set<String> telefones = new HashSet<>();
 
-	public Integer getId() {
-		return id;
-	}
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "PERFIS")
+    private Set<Integer> perfis = new HashSet<>();
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    @JsonIgnore
+    @OneToMany(mappedBy = "cliente")
+    private List<Pedido> pedidos = new ArrayList<>();
 
-	public String getNome() {
-		return nome;
-	}
+    public Cliente() {
+        addPerfil(Perfil.CLIENTE);
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
+        super();
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.cpfOuCnpj = cpfOuCnpj;
+        this.tipo = (tipo == null) ? null : tipo.getCod();
+        this.senha = senha;
+        addPerfil(Perfil.CLIENTE);
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getCpfOuCnpj() {
-		return cpfOuCnpj;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setCpfOuCnpj(String cpfOuCnpj) {
-		this.cpfOuCnpj = cpfOuCnpj;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public TipoCliente getTipo() {
-		return TipoCliente.toEnum(tipo);
-	}
+    public String getEmail() {
+        return email;
+    }
 
-	public void setTipo(TipoCliente tipo) {
-		this.tipo = tipo.getCod();
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	public List<Endereco> getEnderecos() {
-		return enderecos;
-	}
+    public String getCpfOuCnpj() {
+        return cpfOuCnpj;
+    }
 
-	public void setEnderecos(List<Endereco> enderecos) {
-		this.enderecos = enderecos;
-	}
+    public void setCpfOuCnpj(String cpfOuCnpj) {
+        this.cpfOuCnpj = cpfOuCnpj;
+    }
 
-	public Set<String> getTelefones() {
-		return telefones;
-	}
+    public TipoCliente getTipo() {
+        return TipoCliente.toEnum(tipo);
+    }
 
-	public void setTelefones(Set<String> telefones) {
-		this.telefones = telefones;
-	}
+    public void setTipo(TipoCliente tipo) {
+        this.tipo = tipo.getCod();
+    }
 
-	public List<Pedido> getPedidos() {
-		return pedidos;
-	}
+    public String getSenha() {
+        return senha;
+    }
 
-	public void setPedidos(List<Pedido> pedidos) {
-		this.pedidos = pedidos;
-	}
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Cliente other = (Cliente) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+    public void addPerfil(Perfil perfil) {
+        perfis.add(perfil.getCod());
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    public Set<String> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(Set<String> telefones) {
+        this.telefones = telefones;
+    }
+
+    public List<Pedido> getPedidos() {
+        return pedidos;
+    }
+
+    public void setPedidos(List<Pedido> pedidos) {
+        this.pedidos = pedidos;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 
 }
