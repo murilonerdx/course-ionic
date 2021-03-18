545719ac72dd484a9f6b70e51dd349da0d42360f
Murilonerdx <mu-silva@outlook.com>
2021-03-18 18:16:49 -0300

    Implementando autentificação e geracao do token JWT

---
+++ src/main/java/com/murilo/cursomc/security/UserSS.java
@@ -0,0 +1,69 @@
+package com.murilo.cursomc.security;
+
+import com.murilo.cursomc.model.cliente.enums.Perfil;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserSS implements UserDetails {
+    private Integer id;
+    private String email;
+    private String senha;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSS(){
+
+    }
+
+    public UserSS(Integer id, String email, String senha, Set<Perfil> perfil) {
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
+        this.authorities = perfil.stream()
+                .map(x->new SimpleGrantedAuthority(x.getDescricao()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public Integer getId(){
+        return id;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
